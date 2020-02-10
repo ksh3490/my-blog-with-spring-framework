59322b7abe62ed9750e86f8412dac90fa01a4946
ksh3490 <ksh349021@gmail.com>
2020-02-10 23:07:32 +0900

    Fix some typo errors

---
+++ src/test/java/com/ksh3490/myblog/MysqlConnectionTest.java
@@ -0,0 +1,34 @@
+package com.ksh3490.myblog;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/dataSource-context.xml" })
+public class MysqlConnectionTest {
+	private static final Logger logger = LoggerFactory.getLogger(MysqlConnectionTest.class);
+	
+	@Inject
+	private DataSource ds;
+	
+	@Test
+	public void testConnection() {
+
+		try (Connection conn = ds.getConnection()){
+			logger.info("\n MySQL 연결 : " + conn);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
