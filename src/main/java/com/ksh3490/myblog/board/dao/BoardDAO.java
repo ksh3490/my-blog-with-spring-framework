2cc4539477973ddc765cf5cb7dad55de033bbe70
ksh3490 <ksh349021@gmail.com>
2020-02-11 18:27:50 +0900

    Create interface BoardDAO.java

---
+++ src/main/java/com/ksh3490/myblog/board/dao/BoardDAO.java
@@ -0,0 +1,17 @@
+package com.ksh3490.myblog.board.dao;
+
+import java.util.List;
+
+import com.ksh3490.myblog.board.model.BoardVO;
+
+public interface BoardDAO {
+	
+	public List<BoardVO> getBoardList() throws Exception;
+	public BoardVO getBoardContent(int bid) throws Exception;
+	public int insertBoard(BoardVO boardVO) throws Exception;
+	public int updateBoard(BoardVO boardVO) throws Exception;
+	public int deleteBoard(int bid) throws Exception;
+	public int updateViewCnt(int bid) throws Exception;
+	
+	
+}
