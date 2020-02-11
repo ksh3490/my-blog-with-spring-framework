96020201836337a246d05d2199804fe54e439714
ksh3490 <ksh349021@gmail.com>
2020-02-11 18:39:49 +0900

    Create BoardDAOImpl.java
    To implement the BoardDAO interface

---
+++ src/main/java/com/ksh3490/myblog/board/dao/BoardDAOImpl.java
@@ -0,0 +1,54 @@
+package com.ksh3490.myblog.board.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.ksh3490.myblog.board.model.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO {
+	
+	@Inject
+	private SqlSession sqlSession;
+
+	@Override
+	public List<BoardVO> getBoardList() throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectList("com.ksh3490.myblog.board.boardMapper.getBoardList");
+	}
+
+	@Override
+	public BoardVO getBoardContent(int bid) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("com.ksh3490.myblog.board.boardMapper.getBoardContent", bid);
+	}
+
+	@Override
+	public int insertBoard(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("com.ksh3490.myblog.board.boardMapper.insertBoard", boardVO);
+	}
+
+	@Override
+	public int updateBoard(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.update("com.ksh3490.myblog.board.boardMapper.updateBoard", boardVO);
+	}
+
+	@Override
+	public int deleteBoard(int bid) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("com.ksh3490.myblog.board.boardMapper.deleteBoard", bid);
+	}
+
+	@Override
+	public int updateViewCnt(int bid) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.update("com.ksh3490.myblog.board.boardMapper.updateViewCnt", bid);
+	}
+
+}
