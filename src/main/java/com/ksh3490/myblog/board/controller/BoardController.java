e4ad56230e4f67f5b6efb3d047f72924c5835083
ksh3490 <ksh349021@gmail.com>
2020-02-13 13:50:29 +0900

    Update BoardController.java
    To locate to boardForm and to save new contents in the board form

--- src/main/java/com/ksh3490/myblog/board/controller/BoardController.java
+++ src/main/java/com/ksh3490/myblog/board/controller/BoardController.java
@@ -4,9 +4,12 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.ksh3490.myblog.board.model.BoardVO;
 import com.ksh3490.myblog.board.service.BoardService;
 
 
@@ -21,6 +24,17 @@ public class BoardController {
 	public String getBoardList(Model model) throws Exception{
 		model.addAttribute("boardList", boardService.getBoardList());
 		return "board/index";
+	}
+	
+	@RequestMapping(value = "/boardForm")
+	public String boardForm() {
+		return "board/boardForm";
+	}
+	
+	@RequestMapping(value = "/saveBoard", method = RequestMethod.POST)
+	public String saveBoard(@ModelAttribute("BoardVO") BoardVO boardVO, RedirectAttributes rttr) throws Exception{
+		boardService.insertBoard(boardVO);
+		return "redirect:/board/getBoardList";
 	}
 
 }
