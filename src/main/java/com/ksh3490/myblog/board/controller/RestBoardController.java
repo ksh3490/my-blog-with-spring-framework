a4104b1b03a1548a83c87b31eb09738a01a780b7
ksh3490 <ksh349021@gmail.com>
2020-03-10 23:00:49 +0900

    Add saveReply method

--- src/main/java/com/ksh3490/myblog/board/controller/RestBoardController.java
+++ src/main/java/com/ksh3490/myblog/board/controller/RestBoardController.java
@@ -1,11 +1,14 @@
 package com.ksh3490.myblog.board.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +29,21 @@ public class RestBoardController {
 	@RequestMapping(value = "/getReplyList", method = RequestMethod.POST)
 	public List<ReplyVO> getReplyList(@RequestParam("bid") int bid) throws Exception{
 		return boardService.getReplyList(bid);
+	}
+	
+	@RequestMapping(value = "/saveReply", method = RequestMethod.POST)
+	public Map<String, Object> saveReply(@RequestBody ReplyVO replyVO) throws Exception {
+		Map<String, Object> result = new HashMap <>();
+		
+		try {
+			boardService.saveReply(replyVO);
+			result.put("status", "OK");
+		}catch(Exception e) {
+			e.printStackTrace();
+			result.put("status", "False");
+		}
+		
+		return result;
 	}
 	
 }
